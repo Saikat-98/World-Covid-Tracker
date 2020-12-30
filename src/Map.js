b14cf5e7737db3436b91047664fa259b43e189e0
Saikat-98 <saikatchoudhury1998@gmail.com>
2020-12-30 22:43:02 +0530

    Fixed bugs and dependency issues

--- src/Map.js
+++ src/Map.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Map as LeafletMap, TileLayer } from 'react-leaflet'
+import { MapContainer as LeafletMap, TileLayer } from 'react-leaflet'
 import 'leaflet-fullscreen/dist/Leaflet.fullscreen.js'
 import './Map.css'
 import { showDataOnMap } from './util'
