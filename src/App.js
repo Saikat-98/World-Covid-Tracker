1b932967e3ca9ee90ad8a4323f854773e9da9dda
Saikat-98 <saikatchoudhury1998@gmail.com>
2021-01-13 18:44:25 +0530

    Changing the multiplier

--- src/App.js
+++ src/App.js
@@ -15,58 +15,58 @@ import { sortData, prettyPrintStat } from "./util";
 import "./App.css";
 import "leaflet/dist/leaflet.css";
 
-function App(props) {
-  const [countries, setCountries] = useState([]);
-  const [country, setCountry] = useState("worldwide");
-  const [countryInfo, setCountryInfo] = useState({});
-  const [tableData, setTableData] = useState([]);
-  const [mapCenter, setMapCenter] = useState([0, 0]);
-  const [mapZoom, setMapZoom] = useState(props.width === "xs" ? 2 : 3);
-  const [mapCountries, setMapCountries] = useState([]);
-  const [casesType, setCasesType] = useState("cases");
+function App ( props ) {
+  const [ countries, setCountries ] = useState( [] );
+  const [ country, setCountry ] = useState( "worldwide" );
+  const [ countryInfo, setCountryInfo ] = useState( {} );
+  const [ tableData, setTableData ] = useState( [] );
+  const [ mapCenter, setMapCenter ] = useState( [ 0, 0 ] );
+  const [ mapZoom, setMapZoom ] = useState( props.width === "xs" ? 2 : 2 );
+  const [ mapCountries, setMapCountries ] = useState( [] );
+  const [ casesType, setCasesType ] = useState( "cases" );
 
-  useEffect(() => {
-    fetch("https://disease.sh/v3/covid-19/all")
-      .then((response) => response.json())
-      .then((data) => {
-        setCountryInfo(data);
-      });
-  }, []);
+  useEffect( () => {
+    fetch( "https://disease.sh/v3/covid-19/all" )
+      .then( ( response ) => response.json() )
+      .then( ( data ) => {
+        setCountryInfo( data );
+      } );
+  }, [] );
 
-  useEffect(() => {
+  useEffect( () => {
     const getCountriesData = async () => {
-      await fetch("https://disease.sh/v3/covid-19/countries")
-        .then((response) => response.json())
-        .then((data) => {
-          const countries = data.map((country) => ({
+      await fetch( "https://disease.sh/v3/covid-19/countries" )
+        .then( ( response ) => response.json() )
+        .then( ( data ) => {
+          const countries = data.map( ( country ) => ( {
             name: country.country,
             value: country.countryInfo.iso2,
-          }));
-          const sortedData = sortData(data);
-          setTableData(sortedData);
-          setMapCountries(data);
-          setCountries(countries);
-        });
+          } ) );
+          const sortedData = sortData( data );
+          setTableData( sortedData );
+          setMapCountries( data );
+          setCountries( countries );
+        } );
     };
     getCountriesData();
-  }, []);
+  }, [] );
 
-  const onCountryChange = async (event) => {
+  const onCountryChange = async ( event ) => {
     let countryCode = event.target.value;
-    setCountry(countryCode);
+    setCountry( countryCode );
 
     const url =
       countryCode === "worldwide"
         ? "https://disease.sh/v3/covid-19/all"
-        : `https://disease.sh/v3/covid-19/countries/${countryCode}`;
-    await fetch(url)
-      .then((response) => response.json())
-      .then((data) => {
-        if (countryCode === "worldwide") setMapCenter([0, 0]);
-        else setMapCenter([data.countryInfo.lat, data.countryInfo.long]);
-        setCountryInfo(data);
-        setMapZoom(props.width === "xs" ? 2 : 3);
-      });
+        : `https://disease.sh/v3/covid-19/countries/${ countryCode }`;
+    await fetch( url )
+      .then( ( response ) => response.json() )
+      .then( ( data ) => {
+        if ( countryCode === "worldwide" ) setMapCenter( [ 0, 0 ] );
+        else setMapCenter( [ data.countryInfo.lat, data.countryInfo.long ] );
+        setCountryInfo( data );
+        setMapZoom( props.width === "xs" ? 2 : 3 );
+      } );
   };
   return (
     <div className="app">
@@ -76,18 +76,18 @@ function App(props) {
           <FormControl className="app__dropdown">
             <Select
               variant="outlined"
-              value={country}
-              onChange={onCountryChange}
+              value={ country }
+              onChange={ onCountryChange }
             >
               <MenuItem value="worldwide">WorldWide</MenuItem>
-              {countries.map((country, index) => (
+              { countries.map( ( country, index ) => (
                 <MenuItem
-                  key={`country-dropdown-${index}`}
-                  value={country.value}
+                  key={ `country-dropdown-${ index }` }
+                  value={ country.value }
                 >
-                  {country.name}
+                  {country.name }
                 </MenuItem>
-              ))}
+              ) ) }
             </Select>
           </FormControl>
         </div>
@@ -95,49 +95,49 @@ function App(props) {
         <div className="app__stats">
           <InfoBox
             isRed
-            active={casesType === "cases"}
+            active={ casesType === "cases" }
             title="Cases"
-            cases={prettyPrintStat(countryInfo.todayCases)}
-            total={prettyPrintStat(countryInfo.cases)}
-            onClick={(e) => setCasesType("cases")}
+            cases={ prettyPrintStat( countryInfo.todayCases ) }
+            total={ prettyPrintStat( countryInfo.cases ) }
+            onClick={ () => setCasesType( "cases" ) }
           />
           <InfoBox
             isGreen
-            active={casesType === "recovered"}
+            active={ casesType === "recovered" }
             title="Recovered"
-            cases={prettyPrintStat(countryInfo.todayRecovered)}
-            total={prettyPrintStat(countryInfo.recovered)}
-            onClick={(e) => setCasesType("recovered")}
+            cases={ prettyPrintStat( countryInfo.todayRecovered ) }
+            total={ prettyPrintStat( countryInfo.recovered ) }
+            onClick={ () => setCasesType( "recovered" ) }
           />
           <InfoBox
             isOrange
-            active={casesType === "deaths"}
+            active={ casesType === "deaths" }
             title="Deaths"
-            cases={prettyPrintStat(countryInfo.todayDeaths)}
-            total={prettyPrintStat(countryInfo.deaths)}
-            onClick={(e) => setCasesType("deaths")}
+            cases={ prettyPrintStat( countryInfo.todayDeaths ) }
+            total={ prettyPrintStat( countryInfo.deaths ) }
+            onClick={ () => setCasesType( "deaths" ) }
           />
         </div>
 
         <Map
-          center={mapCenter}
-          zoom={mapZoom}
-          countries={mapCountries}
-          casesType={casesType}
-          minZoom={mapZoom}
+          center={ mapCenter }
+          zoom={ mapZoom }
+          countries={ mapCountries }
+          casesType={ casesType }
+          minZoom={ mapZoom }
         />
       </div>
 
       <Card className="app__right">
         <CardContent>
           <h3>Live Cases by Country</h3>
-          <Table countries={tableData} />
-          <h3 className="app__graphTitle">Worldwide new {casesType}</h3>
-          <LineGraph className="app__graph" casesType={casesType} />
+          <Table countries={ tableData } />
+          <h3 className="app__graphTitle">Worldwide new { casesType }</h3>
+          <LineGraph className="app__graph" casesType={ casesType } />
         </CardContent>
       </Card>
     </div>
   );
 }
 
-export default withWidth()(App);
+export default withWidth()( App );
--- src/util.js
+++ src/util.js
@@ -7,76 +7,76 @@ export const casesTypeColors = {
     hex: "#CC1034",
     rgb: "rgb(204,16,52)",
     half_op: "rgba(204,16,52,0.5)",
-    multiplier: 800,
+    multiplier: 300,
   },
   recovered: {
     hex: "#7dd71d",
     rgb: "rgb(125,215,29)",
     half_op: "rgba(125,215,29,0.5)",
-    multiplier: 1200,
+    multiplier: 400,
   },
   deaths: {
     hex: "#fb4443",
     rgb: "rgb(258,68,67)",
     half_op: "rgba(258,68,67,0.5)",
-    multiplier: 2000,
-  },
+    multiplier: 1200,
+  }
 };
 
-export const sortData = (data) => {
-  const sortedData = [...data];
-  sortedData.sort((a, b) => {
-    if (a.cases > b.cases) return -1;
+export const sortData = ( data ) => {
+  const sortedData = [ ...data ];
+  sortedData.sort( ( a, b ) => {
+    if ( a.cases > b.cases ) return -1;
     return 1;
-  });
+  } );
   return sortedData;
 };
 
-export const prettyPrintStat = (stat) =>
-  stat ? `+${numeral(stat).format("0.0a")}` : "+0";
+export const prettyPrintStat = ( stat ) =>
+  stat ? `+${ numeral( stat ).format( "0.0a" ) }` : "+0";
 
-export const showDataOnMap = (data, caseType = "cases") => {
-  data.sort((a, b) => {
-    if (a[caseType] > b[caseType]) return -1;
+export const showDataOnMap = ( data, caseType ) => {
+  // console.log( caseType, casesTypeColors[ caseType ].hex )
+  data.sort( ( a, b ) => {
+    if ( a[ caseType ] > b[ caseType ] ) return -1;
     return 1;
-  });
-  const topFiveCountries = data.slice(0, 5).map((e) => e.countryInfo.iso3);
-  return data.map((country, index) => {
+  } );
+  const topFiveCountries = data.slice( 0, 5 ).map( ( e ) => e.countryInfo.iso3 );
+  return data.map( ( country, index ) => {
     return (
       <Circle
-        center={[country.countryInfo.lat, country.countryInfo.long]}
-        key={`map-circle-${index}`}
-        fillOpacity={0.4}
-        color={casesTypeColors[caseType].hex}
-        fillColor={casesTypeColors[caseType].hex}
+        center={ [ country.countryInfo.lat, country.countryInfo.long ] }
+        key={ `map-circle-${ index }` }
+        fillOpacity={ 0.4 }
+        color={ casesTypeColors[ caseType ].hex }
+        fillColor={ casesTypeColors[ caseType ].hex }
         radius={
-          Math.sqrt(country[caseType]) * casesTypeColors[caseType].multiplier
+          Math.sqrt( country[ caseType ] ) * casesTypeColors[ caseType ].multiplier
         }
-        className={`${
-          topFiveCountries.includes(country.countryInfo.iso3)
-            ? "map__circle"
-            : null
-        } `}
+        className={ `${ topFiveCountries.includes( country.countryInfo.iso3 )
+          ? "map__circle"
+          : null
+          } ` }
       >
         <Popup>
           <div className="info-container">
             <div
               className="info-flag"
-              style={{ backgroundImage: `url(${country.countryInfo.flag})` }}
+              style={ { backgroundImage: `url(${ country.countryInfo.flag })` } }
             ></div>
-            <div className="info-name">{country.country}</div>
+            <div className="info-name">{ country.country }</div>
             <div className="info-cases">
-              Cases: {numeral(country.cases).format("0,0")}
+              Cases: { numeral( country.cases ).format( "0,0" ) }
             </div>
             <div className="info-recovered">
-              Recovered: {numeral(country.recovered).format("0,0")}
+              Recovered: { numeral( country.recovered ).format( "0,0" ) }
             </div>
             <div className="info-deaths">
-              Deaths: {numeral(country.deaths).format("0,0")}
+              Deaths: { numeral( country.deaths ).format( "0,0" ) }
             </div>
           </div>
         </Popup>
       </Circle>
     );
-  });
+  } );
 };
